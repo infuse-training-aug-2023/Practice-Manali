09f9ed9e5e3c23c7e79feaaa282695ea2e9661fc
manalipotekar03 <manali.potekar@infuse.it>
2023-08-17 14:19:05 +0530

    Created a calculator.cpp file with addition functionality

---
+++ calculator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+class Calculator {
+public:
+    int add(int a, int b) {
+        return a + b;
+    }
+};
+
+int main() {
+    Calculator calc;
+
+    int num1, num2;
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
+    int result = calc.add(num1, num2);
+    std::cout << "The sum is: " << result << std::endl;
+
+    return 0;
+}
