d479863276f2a57277bebecb7ab18ae008a91e3f
manalipotekar03 <manali.potekar@infuse.it>
2023-08-23 16:39:35 +0530

    changed ex-1 added validations

--- ruby/ex2.rb
+++ ruby/ex2.rb
@@ -1,5 +1,9 @@
 class ArrayManipulator
     def element_at(arr, index)
+      if index>arr.length-1
+        puts "Out of index"
+        exit
+      end
       return arr[index]
     end
   
@@ -20,18 +24,18 @@ class ArrayManipulator
   arr = [9, 5, 1, 2, 3, 4, 0, -1]
   array_manipulator = ArrayManipulator.new
   
-  index = 2  
+  index = 8 
   element = array_manipulator.element_at(arr, index)
   puts "Element at index #{index}: #{element}"
   
-  start_pos = 2  
-  end_pos = 5    
-  elements = array_manipulator.inclusive_range(arr, start_pos, end_pos)
-  puts "Elements between positions #{start_pos} and #{end_pos}: #{elements}"
+  # start_pos = 2  
+  # end_pos = 5    
+  # elements = array_manipulator.inclusive_range(arr, start_pos, end_pos)
+  # puts "Elements between positions #{start_pos} and #{end_pos}: #{elements}"
   
-  elements = array_manipulator.non_inclusive(arr, start_pos, end_pos)
-  puts "Elements between positions #{start_pos} (inclusive) and #{end_pos} (exclusive): #{elements}"
+  # elements = array_manipulator.non_inclusive(arr, start_pos, end_pos)
+  # puts "Elements between positions #{start_pos} (inclusive) and #{end_pos} (exclusive): #{elements}"
   
-  length = array_manipulator.start_and_length(arr, start_pos)
-  puts "Length of array from position #{start_pos}: #{length}"
+  # length = array_manipulator.start_and_length(arr, start_pos)
+  # puts "Length of array from position #{start_pos}: #{length}"
   
